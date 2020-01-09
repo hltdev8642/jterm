44f41ca7c707e21d3f3c69cf54427009a76b9f85
Demo User <demouser@Demos-MBP.home>
2020-01-08 22:11:12 -0500

    scarnesdice working correctly (I think)

--- scarnesdice/app/src/main/java/com/hltdev/scarnesdice/MainActivity.java
+++ scarnesdice/app/src/main/java/com/hltdev/scarnesdice/MainActivity.java
@@ -16,7 +16,8 @@ public class MainActivity extends AppCompatActivity {
     private int userTurnScore = 0;
     private int computerOverallScore = 0;
     private int computerTurnScore = 0;
-    private boolean isHold = false;
+    private boolean computerHold = false;
+    private boolean userHold = false;
     private Button btnRoll,btnReset,btnHold;
 
     @Override
@@ -46,10 +47,12 @@ public class MainActivity extends AppCompatActivity {
         });
 
         btnReset = (Button)findViewById(R.id.btnReset);
+
         btnReset.setOnClickListener(new View.OnClickListener()
         {
             Button userScore = (Button)findViewById(R.id.btnScoreNum);
             Button computerScore = (Button)findViewById(R.id.btnScoreCompNum);
+            Button scoreTurnNum = (Button)findViewById(R.id.btnScoreTurnNum);
             Button btnFinalScoreText = (Button) findViewById(R.id.btnFinalScore);
 
 
@@ -57,13 +60,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
+                resetAllScores();
+                /*
                 userOverallScoreState = 0;
                 computerOverallScore = 0;
                 userTurnScore = 0;
                 computerTurnScore = 0;
                 userScore.setText(String.format("%s",userOverallScoreState));
                 computerScore.setText(String.format("%s",computerOverallScore));
-                btnFinalScoreText.setText(String.format("%s", " "));
+                scoreTurnNum.setText(String.format("%s",userTurnScore));
+                btnFinalScoreText.setText(String.format("%s", ""));
+                 */
             }
 
 
@@ -79,18 +86,34 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                isHold = true;
+                userHold = true;
+                userOverallScoreState+=userTurnScore;
                 userScore.setText(String.format("%s",userOverallScoreState));
                 userTurnScore = 0;
                 turnScore.setText(String.format("%s",userTurnScore));
 
                 computerScore.setText(String.format("%s",computerOverallScore));
-                computerTurnScore = 0;
+                //computerTurnScore = 0;
             }
         });
-
+           // resetAllScores();
     }
+    public void resetAllScores ()
+    {
+        Button userScore = (Button)findViewById(R.id.btnScoreNum);
+        Button computerScore = (Button)findViewById(R.id.btnScoreCompNum);
+        Button scoreTurnNum = (Button)findViewById(R.id.btnScoreTurnNum);
+        Button btnFinalScoreText = (Button) findViewById(R.id.btnFinalScore);
 
+        userOverallScoreState = 0;
+        computerOverallScore = 0;
+        userTurnScore = 0;
+        computerTurnScore = 0;
+        userScore.setText(String.format("%s",userOverallScoreState));
+        computerScore.setText(String.format("%s",computerOverallScore));
+        scoreTurnNum.setText(String.format("%s",userTurnScore));
+        btnFinalScoreText.setText(String.format("%s", ""));
+    }
     public boolean compWins (int youscore, int compscore)
     {
         return (compscore > youscore);
@@ -109,8 +132,6 @@ public class MainActivity extends AppCompatActivity {
         if (status == 0)
         {
             Log.d("Winner is: ", "user" );
-            /*btnTurnScore.setText(String.format("%s","Winner"));
-            btnComputerTurnScore.setText(String.format("%s","Loser"));*/
             btnFinalScoreText.setText(String.format("%s","User Wins!"));
             return;
         }
@@ -118,19 +139,11 @@ public class MainActivity extends AppCompatActivity {
        else if (status == 1)
         {
             Log.d("Winner is: ", "computer" );
-          /*  btnTurnScore.setText(String.format("%s","Loser"));
-            btnComputerTurnScore.setText(String.format("%s","Winner"));*/
             btnFinalScoreText.setText(String.format("%s","Computer Wins!"));
 
             return;
 
         }
-/*
-
-        // Button userScore = (Button)findViewById(R.id.btnScoreNum);
-        Button btnTurnScore = (Button) findViewById(R.id.btnScoreTurnNum);
-        Button btnComputerTurnScore = (Button) findViewById(R.id.btnScoreCompNum);
-*/
 
         ImageView dice = (ImageView) findViewById(R.id.imageView);
 
@@ -140,44 +153,45 @@ public class MainActivity extends AppCompatActivity {
             int randomIndex = ((int) (Math.random() * 6 + 1));
             if (randomIndex == 1) {
                 dice.setImageResource(R.drawable.dice1);
-                userTurnScore = 0;
+                if(!userHold){
+                userTurnScore = 0;}
                 btnTurnScore.setText(String.format("%s", userTurnScore));
             } else {
                 switch (randomIndex) {
                     case 2:
                         dice.setImageResource(R.drawable.dice2);
-                        userOverallScoreState += randomIndex;
+//                        userOverallScoreState += randomIndex;
                         userTurnScore += randomIndex;
                         btnTurnScore.setText(String.format("%s", userTurnScore));
-                        isHold = false;
+                         userHold = false;
                         break;
                     case 3:
                         dice.setImageResource(R.drawable.dice3);
-                        userOverallScoreState += randomIndex;
+//                        userOverallScoreState += randomIndex;
                         userTurnScore += randomIndex;
                         btnTurnScore.setText(String.format("%s", userTurnScore));
-                        isHold = false;
+                        userHold = false;
                         break;
                     case 4:
                         dice.setImageResource(R.drawable.dice4);
-                        userOverallScoreState += randomIndex;
+//                        userOverallScoreState += randomIndex;
                         userTurnScore += randomIndex;
                         btnTurnScore.setText(String.format("%s", userTurnScore));
-                        isHold = false;
+                        userHold = false;
                         break;
                     case 5:
                         dice.setImageResource(R.drawable.dice5);
-                        userOverallScoreState += randomIndex;
+//                        userOverallScoreState += randomIndex;
                         userTurnScore += randomIndex;
                         btnTurnScore.setText(String.format("%s", userTurnScore));
-                        isHold = false;
+                        userHold = false;
                         break;
                     case 6:
                         dice.setImageResource(R.drawable.dice6);
-                        userOverallScoreState += randomIndex;
+//                        userOverallScoreState += randomIndex;
                         userTurnScore += randomIndex;
                         btnTurnScore.setText(String.format("%s", userTurnScore));
-                        isHold = false;
+                        userHold = false;
                         break;
 
                 }
@@ -186,52 +200,62 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (type == 1) {
-            if (computerTurnScore > 20) {
-                isHold = true;
+            if (computerTurnScore < 20) {
+                computerOverallScore+=computerTurnScore;
+                computerTurnScore = 0;
+                computerHold = false;
+            }
+            else {
+                computerHold = true;
             }
 
 
                 int randomIndex = ((int) (Math.random() * 6 + 1));
                 if (randomIndex == 1) {
                     dice.setImageResource(R.drawable.dice1);
-                    computerTurnScore = 0;
-                    btnComputerTurnScore.setText(String.format("%s", computerTurnScore));
+                    if(!computerHold) {
+                        //computerOverallScore+=computerTurnScore;
+                        computerTurnScore = 0;
+                        //computerHold=false;}
+                    }
+
+                        btnComputerTurnScore.setText(String.format("%s", computerOverallScore));
                 } else {
                     switch (randomIndex) {
                         case 2:
                             dice.setImageResource(R.drawable.dice2);
-                            computerOverallScore += randomIndex;
+                            //computerOverallScore += randomIndex;
                             computerTurnScore += randomIndex;
-                            btnComputerTurnScore.setText(String.format("%s", computerTurnScore));
-                            isHold = false;
+                            btnComputerTurnScore.setText(String.format("%s", computerOverallScore));
+                            computerHold = false;
                             break;
                         case 3:
                             dice.setImageResource(R.drawable.dice3);
-                            computerOverallScore += randomIndex;
+                            //computerOverallScore += randomIndex;
                             computerTurnScore += randomIndex;
-                            btnComputerTurnScore.setText(String.format("%s", computerTurnScore));
-                            isHold = false;
+                            btnComputerTurnScore.setText(String.format("%s", computerOverallScore));
+                            computerHold = false;
                             break;
                         case 4:
                             dice.setImageResource(R.drawable.dice4);
-                            computerOverallScore += randomIndex;
+                           // computerOverallScore += randomIndex;
                             computerTurnScore += randomIndex;
-                            btnComputerTurnScore.setText(String.format("%s", computerTurnScore));
-                            isHold = false;
+                            btnComputerTurnScore.setText(String.format("%s", computerOverallScore));
+                            computerHold = false;
                             break;
                         case 5:
                             dice.setImageResource(R.drawable.dice5);
-                            computerOverallScore += randomIndex;
+                            //computerOverallScore += randomIndex;
                             computerTurnScore += randomIndex;
-                            btnComputerTurnScore.setText(String.format("%s", computerTurnScore));
-                            isHold = false;
+                            btnComputerTurnScore.setText(String.format("%s", computerOverallScore));
+                            computerHold = false;
                             break;
                         case 6:
                             dice.setImageResource(R.drawable.dice6);
-                            computerOverallScore += randomIndex;
+                            //computerOverallScore += randomIndex;
                             computerTurnScore += randomIndex;
-                            btnComputerTurnScore.setText(String.format("%s", computerTurnScore));
-                            isHold = false;
+                            btnComputerTurnScore.setText(String.format("%s", computerOverallScore));
+                            computerHold = false;
                             break;
 
                     }
@@ -243,12 +267,13 @@ public class MainActivity extends AppCompatActivity {
         }
     public int checkWinner (int scoreA, int scoreB)
     {
-        if ((scoreA > 100 && scoreA > scoreB))
+//        if ( ( (scoreA >= 100) & (scoreA > 0) ) | (scoreA > scoreB)
+        if ((scoreA >= 100) & (scoreA > 0) & (scoreA > scoreB))
         {
             return 0;
         }
 
-        else if ((scoreB > 100 && scoreB > scoreA))
+         if ((scoreB >= 100) & (scoreB > 0) & (scoreB > scoreA))
 
         {
             return 1;
