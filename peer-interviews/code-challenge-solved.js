ae54c55a53667fe353f59e08fcbc7df2a7e34366
hltdev8642 <hltdev8642@gmail.com>
2020-01-10 15:58:19 -0500

    code challenge/interview question 1 working solution

---
+++ peer-interviews/code-challenge-solved.js
@@ -0,0 +1,31 @@
+var map1 = new Map();
+
+var str = "abcbcsdofa";
+
+console.log("Input String:",str);
+var tmp = "";
+var strArr = str.toLowerCase().split("");
+var numArr = new Array(strArr.length);
+for (var i in numArr)
+{
+ numArr[i] = 0;
+}
+
+for (var i in strArr)
+{
+  map1.set(strArr[i],0 );
+}
+
+for (var i in strArr)
+{
+ if(map1.get(strArr[i])>=1)
+ {
+    // console.log(map1.get(strArr[i]));
+    tmp = strArr[i];
+     break;
+ }
+ var value = map1.get(strArr[i]);
+ map1.set(strArr[i],value+1);
+
+}
+console.log(tmp)
